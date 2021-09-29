f27ee02b77bf8afaf274cb646cf77e85d0438c2c
紫橙雨 <1841230544@qq.com>
2021-09-29 14:49:40 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">{{ count }}</router-link>
+    <n-button type="Error" size="tiny">点击我</n-button>
+    <n-divider></n-divider>
   </div>
   <router-view/>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { defineComponent } from "vue";
 
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const count = ref<string>(10);
+</script>
+
+<style lang="less" scoped>
 #nav {
-  padding: 30px;
-
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    color: red;
   }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {
+  // create naive ui
+  create,
+  // component
+  NButton,
+  NDivider
+} from 'naive-ui'
 
-createApp(App).use(store).use(router).mount('#app')
+const native = create({
+  components: [
+    NButton,
+    NDivider
+  ],
+})
+
+createApp(App).use(store).use(router).use(native).mount('#app')
