8f4015fe74d65b4b6c6ec65af335330b1e1463f4
紫橙雨 <1841230544@qq.com>
2021-09-30 16:50:19 +0800

    首页

---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>嘻嘻嘻嘻</p>
+    <router-link to="/d">导航</router-link>
+    <n-button>点击</n-button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: 'index',
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
