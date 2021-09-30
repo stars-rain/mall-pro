b1b259f14317a7ab26127f1e3cedcd70feb1c1aa
紫橙雨 <1841230544@qq.com>
2021-09-30 16:23:28 +0800

    App组件

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Homes</router-link> |
-    <router-link to="/about">{{ count }}嘻嘻嘻</router-link>
-    <n-button type="Error" size="tiny">点击wo</n-button>
-    <n-divider></n-divider>
-  </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-})
-</script>
-
-<script lang="ts" setup>
-import { ref } from "vue";
-
-const count = ref<number>(10);
+  name: "App",
+});
 </script>
 
 <style lang="less" scoped>
-#nav {
-  a {
-    color: red;
-  }
-}
 </style>
