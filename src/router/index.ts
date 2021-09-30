6e12bc62145c6706f8d2860d8f122c8c2dfd40b4
紫橙雨 <1841230544@qq.com>
2021-09-30 16:21:48 +0800

    路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: Array<RouteRecordRaw> = [
+  {
+    /* 404页面 */
+    name: 'NotFound',
+    path: '/:routePath(.+)+',
+    component: () => import('@/views/404.vue'),
+  },
+  {
+    /* 首页 */
+    name: 'index',
+    path: '/',
+    component: () => import('@/views/index.vue'),
+  }
 ]
 
 const router = createRouter({
