0c31fdce50ef474710a2d7e9cb3e40eb2b57aec6
紫橙雨 <1841230544@qq.com>
2021-10-01 23:53:45 +0800

    vue的可选配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 module.exports = {
-    chainWebpack: config => {
-      // svg rule loader
-      const svgRule = config.module.rule('svg') // 找到svg-loader
-      svgRule.uses.clear() // 清除已有的loader, 如果不这样做会添加在此loader之后
-      svgRule.exclude.add(/node_modules/) // 正则匹配排除node_modules目录
-      svgRule.test(/\.svg$/).use('svg-sprite-loader').loader('svg-sprite-loader').options({ symbolId: 'icon-[name]' })
+  chainWebpack: config => {
+    // svg rule loader
+    const svgRule = config.module.rule('svg') // 找到svg-loader
+    svgRule.uses.clear() // 清除已有的loader, 如果不这样做会添加在此loader之后
+    svgRule.exclude.add(/node_modules/) // 正则匹配排除node_modules目录
+    svgRule.test(/\.svg$/).use('svg-sprite-loader').loader('svg-sprite-loader').options({ symbolId: 'icon-[name]' })
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, 'src/less/main.less')]
     }
+  },
 }
