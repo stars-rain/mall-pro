e68f3e2e65984e864e316a2f967c0b96815b45e6
紫橙雨 <1841230544@qq.com>
2021-10-01 10:30:03 +0800

    可选的配置文件

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    chainWebpack: config => {
+      // svg rule loader
+      const svgRule = config.module.rule('svg') // 找到svg-loader
+      svgRule.uses.clear() // 清除已有的loader, 如果不这样做会添加在此loader之后
+      svgRule.exclude.add(/node_modules/) // 正则匹配排除node_modules目录
+      svgRule.test(/\.svg$/).use('svg-sprite-loader').loader('svg-sprite-loader').options({ symbolId: 'icon-[name]' })
+    }
+}
