dcb85b5cdedd494e5845c8624ab3102750e43a00
紫橙雨 <1841230544@qq.com>
2021-10-01 10:56:48 +0800

    axios配置

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -6,7 +6,7 @@ export default {
         axios.defaults.baseURL = 'http://106.15.200.157:8080';
         /* 设置axios网络超时时间 */
         axios.defaults.timeout = 30000;
-        /* 对返回的数据进行拦截处理
+        /* 对返回的数据进行拦截处理 
         axios.interceptors.response.use(res => res.data); */
     }
 }
