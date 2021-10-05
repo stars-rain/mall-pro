ccee2e73871a4ec8c20a2a3d41fb2a2f28bc07a0
紫橙雨 <1841230544@qq.com>
2021-10-05 16:02:09 +0800

    axios配置插件

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,12 +1,14 @@
+import type { createApp } from "@vue/runtime-dom";
 import axios from "axios"
 
 export default {
-    install(): void {
+    install(app: ReturnType<typeof createApp>): void {
         /* 此url会自动加到请求地址的开头 */
         axios.defaults.baseURL = 'http://106.15.200.157:8080';
         /* 设置axios网络超时时间 */
         axios.defaults.timeout = 30000;
         /* 对返回的数据进行拦截处理 
         axios.interceptors.response.use(res => res.data); */
+        app.config.globalProperties.$axios = axios; // 将axios注入app实例
     }
 }
