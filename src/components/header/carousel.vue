006bff4556d0f71e3f25bd7ea17f46e31e96fb5b
紫橙雨 <1841230544@qq.com>
2021-10-05 17:40:57 +0800

    头部商品轮播组件

---
+++ src/components/header/carousel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="carousel">
+      哈哈哈
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+
+})
+</script>
+
+<style lang="less" scoped>
+
+</style>
