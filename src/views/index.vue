974c9de15c267877f4b76ab533a4b65c48859ae4
紫橙雨 <1841230544@qq.com>
2021-10-05 23:36:52 +0800

    商城首页

--- src/views/index.vue
+++ src/views/index.vue
@@ -3,13 +3,13 @@
     <!-- 商城头部组件 -->
     <mall-header></mall-header>
     <div class="container">
-      <mall-header-carousel></mall-header-carousel>
+      <mall-header-carousel :datas="carouselData"></mall-header-carousel>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, onBeforeMount } from "vue";
 import mallHeader from "@/components/header/header.vue";
 import mallHeaderCarousel from "@/components/header/carousel.vue";
 
@@ -19,6 +19,22 @@ export default defineComponent({
     mallHeader,
     mallHeaderCarousel,
   },
+});
+</script>
+
+<script lang="ts" setup>
+import axios from "axios";
+
+/**
+ * 头部轮播图数据
+ */
+// eslint-disable-next-line no-undef
+const carouselData = ref<Array<Carousel>>([]);
+
+onBeforeMount(async () => {
+  /* 发送请求轮播图数据 */
+  const { data } = await axios.get("/slideshow");
+  carouselData.value = data;
 });
 </script>
 
