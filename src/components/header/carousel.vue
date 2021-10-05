427c5150dc967a8672fe84fbfff6ac6baac90ce0
紫橙雨 <1841230544@qq.com>
2021-10-05 23:38:04 +0800

    头部商品轮播组件

--- src/components/header/carousel.vue
+++ src/components/header/carousel.vue
@@ -1,17 +1,51 @@
 <template>
-  <div class="carousel">
-      哈哈哈
-  </div>
+  <ul class="carousel">
+    <li class="carousel-item" v-for="item in datas" :key="item.id">
+      <a href="javascript:void(0);">
+        <div class="item-img">
+          <img :src="item.imgSrc" alt="无法加载此图片" />
+        </div>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, defineProps, PropType } from "vue";
 
-export default defineComponent({
+export default defineComponent({});
+</script>
 
-})
+<script lang="ts" setup>
+/* -1226px -2452px -3678px -3678px */
+defineProps({
+  datas: {
+    // eslint-disable-next-line no-undef
+    type: Array as PropType<Array<Carousel>>,
+    required: true,
+  },
+});
 </script>
 
 <style lang="less" scoped>
+.carousel {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: hidden;
 
+  &-item {
+    height: 460px;
+
+    .item {
+      &-img {
+        width: 1226px;
+        height: 460px;
+
+        img {
+          .widHei();
+        }
+      }
+    }
+  }
+}
 </style>
