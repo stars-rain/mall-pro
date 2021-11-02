e3ecd59e8da752e0a6eebf5eaa715834d9b84f4c
紫橙雨 <1841230544@qq.com>
2021-11-02 18:05:34 +0800

    根部vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,55 +1,79 @@
-import { InjectionKey } from 'vue'
+import type { InjectionKey } from 'vue'
 import { createStore, useStore as baseStore, Store } from 'vuex'
 import { $axios } from '@/plugins/axios'
-
-export interface State {
-  /**
-   * 头部商品分类导航数据
-   */
-  commodityTypesDatas: Array<CommodityTypes>,
-  /**
-   * 头部轮播图数据
-   */
-  carouselDatas: Array<Carousel>,
-}
+import UserModule from "./modules/user"
+import type { RootState, AllState } from './interface' 
 
 /* 定义类型化的 InjectionKey */
-export const key: InjectionKey<Store<State>> = Symbol()
+export const key: InjectionKey<Store<RootState>> = Symbol()
 
-export default createStore<State>({
+/**
+ * 请求数据的一个封装
+ * @param url - 请求的url
+ * @param method - 请求的方法
+ * @param params - 请求的参数
+ * @returns 带有vuex根部state属性和vuex根部的state属性内的对象属性参数的一个async函数
+ */
+function $http<T>(
+  url: string,
+  method: 'get' | 'post' = 'get',
+  params?: T): (state: RootState,
+    type: keyof RootState,) => Promise<boolean> {
+  return async (state: RootState,
+    type: keyof RootState,): Promise<boolean> => {
+    try {
+      const { data } = await $axios([url, method, params]);
+      state[type] = data;
+      if (state[type]) return Promise.resolve(true)
+      return Promise.reject(false)
+    } catch (error) {
+      console.log(error);
+      return Promise.reject(false)
+    }
+  }
+}
+
+export default createStore<RootState>({
   state: () => ({
     commodityTypesDatas: [],
-    carouselDatas: []
+    carouselDatas: [],
+    phoneDatas: {} as Phone,
+    homeAppliances: {} as Phone,
   }),
   mutations: {
+  },
+  actions: {
     /**
      * 请求头部商品分类导航数据
      * @param state - 根部state属性
      */
-    async getCommodityTypesDatas(state): Promise<void> {
-      try {
-        const { data } = await $axios('/head');
-        state.commodityTypesDatas = data;
-      } catch (error) {
-        console.log(error);
-      }
+    getCommodityTypesDatas({ state }): Promise<boolean> {
+      return $http('/head')(state, 'commodityTypesDatas')
     },
     /**
      * @param state - 根部state属性
      */
-    async getCarouselDatas(state): Promise<void> {
-      try {
-        const { data } = await $axios('/slideshow');
-        state.carouselDatas = data;
-      } catch (error) {
-        console.log(error);
-      }
+    getCarouselDatas({ state }): Promise<boolean> {
+      return $http('/slideshow')(state, 'carouselDatas');
+    },
+    /**
+     * 请求手机模块的数据
+     * @param state - 根部state属性
+     */
+    getPhoneDatas({ state }): Promise<boolean> {
+      return $http('/body', 'get', { title: '手机' })(state, 'phoneDatas');
+    },
+    /**
+     * 请求家电模块的数据
+     * @param state - 根部state属性
+     */
+    getHomeAppliances({ state }): Promise<boolean> {
+      return $http('/body', 'get', { title: '家电' })(state, 'homeAppliances');
     },
   },
-  actions: {
-  },
   modules: {
-  }
+    UserModule,
+  },
 })
 
-export const useStore = (): Store<State> => baseStore(key);
+export const useStore = <T = AllState>(): Store<T> => baseStore<T>(key);
