dbf9181d7b1d3913383ec7dbfaed48171a7ac5a5
紫橙雨 <1841230544@qq.com>
2021-11-02 18:06:17 +0800

    全局vuex声明文件

--- src/store/module.d.ts
+++ src/store/module.d.ts
@@ -1,11 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ComponentCustomProperties } from 'vue'
-import { Store } from 'vuex'
-import { State } from "./index"
+import type { ComponentCustomProperties } from 'vue'
+import type { Store } from 'vuex'
+import type { AllState } from "./interface"
 
 declare module '@vue/runtime-core' {
     // 为 `this.$store` 提供类型声明
     interface ComponentCustomProperties {
-        $store: Store<State>
+        $store: Store<AllState>
     }
 }
