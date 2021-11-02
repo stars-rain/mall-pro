e02db090eda0c0e27b605e6e73e07c94f96a3a2a
紫橙雨 <1841230544@qq.com>
2021-11-02 17:07:30 +0800

    vue的可选配置文件

--- vue.config.js
+++ vue.config.js
@@ -16,4 +16,18 @@ module.exports = {
       patterns: [path.resolve(__dirname, 'src/less/main.less')]
     }
   },
+  devServer: {
+    proxy: {
+      '/api': {
+        //代理api
+        target: 'http://106.15.200.157:8080', // 代理接口(注意只要域名就够了)
+        changeOrigin: true, //是否跨域
+        ws: true, // proxy websockets
+        pathRewrite: {
+          //重写路径
+          '^/api': '' //代理路径
+        }
+      },
+    },
+  },
 }
