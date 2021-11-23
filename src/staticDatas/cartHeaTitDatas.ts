5a10a6374b79becf0c8ae24ed7156789e4d2a0ca
紫橙雨 <1841230544@qq.com>
2021-11-23 14:59:31 +0800

    购物车页面的商品头部标签的静态数据

---
+++ src/staticDatas/cartHeaTitDatas.ts
@@ -0,0 +1,38 @@
+const headerDatas: Array<{
+    id: number;
+    className: string;
+    title: string;
+}> = [
+    {
+        id: 1,
+        className: 'cart-commodity__all clearfix',
+        title: '全选',
+    },
+    {
+        id: 2,
+        className: 'cart-commodity__mess',
+        title: '商品信息',
+    },
+    {
+        id: 3,
+        className: 'cart-commodity__price',
+        title: '单价',
+    },
+    {
+        id: 4,
+        className: 'cart-commodity__count',
+        title: '数量',
+    },
+    {
+        id: 5,
+        className: 'cart-commodity__cost',
+        title: '小计',
+    },
+    {
+        id: 6,
+        className: 'cart-commodity__opeartion',
+        title: '操作',
+    },
+];
+
+export default headerDatas
