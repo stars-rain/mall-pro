c9a4fe030cae2454ac81562d52c1bdbc2c5f4390
紫橙雨 <1841230544@qq.com>
2021-11-23 14:20:49 +0800

    ts全局声明文件

--- src/global.d.ts
+++ src/global.d.ts
@@ -28,24 +28,61 @@ declare type CommodityTypes = {
  */
 declare type Carousel = {
     id: number;
-    url: string;
+    productId: number;
     imgSrc: string;
 }
 
 /**
  * 手机类型数据声明
  */
-type Product1 = Pick<Commodity, 'id' | 'titleId' | 'imgSrc'> & { procuctId: number };
+type Product1 = Pick<Commodity, 'id' | 'titleId' | 'imgSrc'> & { productId: number };
 
+/**
+ * 首页商品数据接口
+ */
 declare type Phone = {
     product1: Product1;
     product2: Product1;
     products: Array<Commodity>;
 }
 
+/**
+ * 用户属性接口
+ */
 declare type User = {
     userName: string;
     account: string;
     password: string;
     repeatPassword: string;
+}
+
+/**
+ * 商品详情的数据接口
+ */
+declare type CommodityDetails = Commodity &
+{ saleDesc: string;imgDetails: Array<{ id: number;src: string; }> };
+
+/**
+ * 购物车中某件商品的接口
+ */
+declare type CartItem = {
+    id: number;
+    productId: number; // 商品的id值(唯一)
+    title: string; // 商品所属类型
+    name: string; // 商品名称
+    imgSrc: string; // 商品图片路径
+    price: number; // 商品单价
+    count: number; // 商品数量
+    cost: number; // 商品总价
+    account?: string;
+}
+
+/**
+ * 评论内容接口
+ */
+declare type Comments = {
+    time: number; // 评论发表的时间戳
+    comment: string; // 评论内容
+    avatar: string; // 头像路径
+    userName: string; // 评论人的昵称
 }
