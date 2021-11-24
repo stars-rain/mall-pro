b1bcf6cc6661d854a78a70886eea95a3bc529414
紫橙雨 <1841230544@qq.com>
2021-11-24 23:55:49 +0800

    vuex的评论模板

--- src/store/modules/comment.ts
+++ src/store/modules/comment.ts
@@ -8,8 +8,20 @@ const CommentModule: Module<CommentState, RootState> = {
     commentList: [], // 评论列表
   }),
   mutations: {
-    addComment(state, payload: Comments): void {
-      state.commentList.push(payload);
+    // 添加评论
+    addComment(state, payload: { content: Comments }): void {
+      state.commentList.push(payload.content);
+    },
+    // 添加回复
+    addReplayComment(state, payload: { index: number, content: Comments & { replaiedName: string } }): void {
+      const comment: Comments = state.commentList[payload.index];
+      comment.replay.push(payload.content);
+    },
+    // 隐藏回复的评论
+    hideReplay(state): void {
+      state.commentList.forEach(item => {
+        if (item.showReplay) item.showReplay = false;
+      })
     },
   },
   actions: {
